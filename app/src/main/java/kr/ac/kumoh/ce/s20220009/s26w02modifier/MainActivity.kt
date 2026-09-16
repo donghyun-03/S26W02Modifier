6d4812ad50f03ea63247fe34a864a28c5c24eb83
donghyun <dhyunkang7056@gmail.com>
2026-09-16 14:35:48 +0900

    🚀 완성

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20220009/s26w02modifier/MainActivity.kt
@@ -0,0 +1,54 @@
+package kr.ac.kumoh.ce.s20220009.s26w02modifier
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kr.ac.kumoh.ce.s20220009.s26w02modifier.ui.theme.S26W02ModifierTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            S26W02ModifierTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(30.dp)
+            .background(Color.Blue),
+        color = Color.Yellow,
+        fontSize = 30.sp,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.ExtraBold,
+        textAlign = TextAlign.Center,
+    )
+}
